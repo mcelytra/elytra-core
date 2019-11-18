36bf79852a409db0f24256547e79230a7f624a1f
LambdAurora <aurora42lambda@gmail.com>
2019-11-18 17:19:55 +0100

    :construction: More WIP on ItemStack.

--- src/main/java/org/mcelytra/core/inventory/ItemStack.java
+++ src/main/java/org/mcelytra/core/inventory/ItemStack.java
@@ -15,9 +15,36 @@ import org.mcelytra.core.item.ItemConvertible;
 public class ItemStack
 {
     private final Item item;
+    private       int  count;
 
     public ItemStack(ItemConvertible item)
     {
+        this(item, 1);
+    }
+
+    public ItemStack(ItemConvertible item, int count)
+    {
         this.item = item == null ? null : item.as_item();
+        this.count = count;
+    }
+
+    /**
+     * Gets the items count in this stack.
+     *
+     * @return The items count in this stack.
+     */
+    public int get_count()
+    {
+        return this.count;
+    }
+
+    /**
+     * Sets the items count in this stack.
+     *
+     * @param count The items count in this stack.
+     */
+    public void set_count(int count)
+    {
+        this.count = count;
     }
 }
